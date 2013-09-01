0ebf653645e59c86165f43dd6c50f908b6309d01
Nate Tate <nate91711@gmail.com>
2013-08-31 21:59:25 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoanItToMe::Application.config.secret_token = '236759b4922e3f20ed0ca8b9544db52a346e7a7819bdf8aef52852121148391355325a03d76939c7f7a002fcbef71c91788eec53e868e1900353bd980546d5dc'
