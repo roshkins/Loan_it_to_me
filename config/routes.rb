7cfb251d09bf8fefe5d19bfbef6bcc51e3f26d73
Nate Tate <nate91711@gmail.com>
2013-09-02 14:04:40 -0700

    added landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 LoanItToMe::Application.routes.draw do
 
-  root to: "sessions#new"
+  root to: "categories#index"
   resources :categories
   resource  :session, only: [:create, :new, :destroy]
   resources :users
