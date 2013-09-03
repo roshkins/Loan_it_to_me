728e1a19ff42891349eb5715e34da8ecfecd3623
Nate Tate <nate91711@gmail.com>
2013-09-02 18:39:05 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Loan It To Me
 
 A simple app to rent tools from neighbors.
+It is designed to be a Single Page App.
+The app starts at the Categories Controller serving the index page.
+Then the LoanItToMe.js script launches backbone and starts the router.
+The categoryIndexView should launch next and display each category.
+
+###Current Progress
+Im not sure why the CategoryIndex view will not display a list of categories.
+
--- app/assets/javascripts/loan_it_to_me.js
+++ app/assets/javascripts/loan_it_to_me.js
@@ -3,13 +3,14 @@ window.LoanItToMe = {
   Collections: {},
   Views: {},
   Routers: {},
-  initialize: function($rootEl, catdata) {
+  initialize: function( $rootEl, catdata ) {
     var categories = new LoanItToMe.Collections.Categories( catdata );
     var categoryRouter = new LoanItToMe.Routers.Category( $rootEl, categories );
+    Backbone.history.start();
   }
 };
 
-$(document).ready(function(){
-  var categories = JSON.parse($('#categories-data').html());
-  LoanItToMe.initialize($('.content'), categories);
+$(document).ready(function() {
+  var categories = JSON.parse( $('#categories-data').html() );
+  LoanItToMe.initialize( $('.content'), categories );
 });
